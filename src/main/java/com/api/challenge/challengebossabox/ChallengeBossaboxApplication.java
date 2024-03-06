91d838a0f0cccb722fb501f5db0edc26d74224ea
wedo <costadefelipe@gmail.com>
2024-03-06 11:08:49 -0300

    config init

---
+++ src/main/java/com/api/challenge/challengebossabox/ChallengeBossaboxApplication.java
@@ -0,0 +1,13 @@
+package com.api.challenge.challengebossabox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeBossaboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeBossaboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/challenge/challengebossabox/ChallengeBossaboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.challenge.challengebossabox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeBossaboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
